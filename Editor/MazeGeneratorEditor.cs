a72bca90cbd8e95d91af0cd06823a6fb7028a49a
Bernhard Miettinen <bernhard.miettinen@gmail.com>
2019-07-21 05:39:57 +0300

    Updated MazeGeneratorEditor to use the dummy base type

---
+++ Editor/MazeGeneratorEditor.cs
@@ -0,0 +1,58 @@
+﻿using Unity.EditorCoroutines.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Hirame.Cybele.Editor
+{
+    [CustomEditor (typeof (GeneratorBase), true)]
+    public class MazeGeneratorEditor : UnityEditor.Editor
+    {
+        private IGenerator generator;
+
+        private EditorCoroutine genRoutine;
+        
+        private void OnEnable ()
+        {
+            generator = target as IGenerator;
+        }
+
+        private void OnDisable ()
+        {
+            if (genRoutine != null)
+                EditorCoroutineUtility.StopCoroutine (genRoutine);
+        }
+
+        public override void OnInspectorGUI ()
+        {
+            if (generator == null)
+            {
+                OnEnable ();
+                return;
+            }
+
+            using (var changed = new EditorGUI.ChangeCheckScope ())
+            {
+                DrawControlButtons ();
+                
+                DrawPropertiesExcluding (serializedObject, "m_Script");
+                
+                if (changed.changed)
+                {
+                    serializedObject.ApplyModifiedProperties ();
+                }
+            }
+        }
+
+        private void DrawControlButtons ()
+        {
+            if (GUILayout.Button ("Generate"))
+            {
+                if (genRoutine != null)
+                    EditorCoroutineUtility.StopCoroutine (genRoutine);
+                
+                genRoutine = EditorCoroutineUtility.StartCoroutine (generator.Generate (), generator);
+            }
+        }
+    }
+
+}
